93eedc3506133a660cae8124bb2577871c2b62b2
Mohsen Azimi <me@azimi.me>
2015-10-09 12:26:20 -0700

    Add failing test for end-to-end stuff

---
+++ test/end-to-end.js
@@ -0,0 +1,73 @@
+'use strict';
+
+import {expect} from 'chai';
+import YAWN from '../src/index.js';
+
+const input = `
+swagger: '2.0'
+info:
+  version: 0.0.0
+  title: Simple API
+paths:
+  /:
+
+    parameters:
+      - name: test
+        in: query
+        type: string
+
+    get:
+      responses:
+        '200':
+          description: OK
+`;
+
+const output = `
+swagger: '2.0'
+info:
+  version: 0.0.0
+  title: Simple API
+paths:
+  /:
+
+    parameters:
+      - name: test
+        in: query
+        type: string
+
+    get:
+      responses:
+        '200':
+          description: OK
+  /newPath:
+    get:
+      responses:
+        200:
+          description: OK
+`;
+
+describe('preserves comments and styling when', ()=> {
+
+  describe('JSON is complex', ()=> {
+    // debugger
+    it('adds new object hash', ()=> {
+      let yawn = new YAWN(input);
+
+      let json = yawn.json;
+
+      json.paths['/newPath'] = {
+        get: {
+          responses: {
+            200: {
+              description: 'OK'
+            }
+          }
+        }
+      };
+
+      yawn.json = json;
+
+      expect(yawn.yaml).to.equal(output);
+    });
+  });
+});
