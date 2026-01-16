0cee8dc033964db280315cd52ae90f5e6e3dd6ab
Mohsen Azimi <mohsen1@users.noreply.github.com>
2026-01-16 09:10:54 -0600

    fix: Build demo to docs/demo for correct URL path

--- build.sh
+++ build.sh
@@ -7,7 +7,11 @@ packageJson=$(cat package.json)
 # we do this due to some limitations in Parcel in which it does not allow a package.json to have main and module entries
 node -e "const fs = require('fs'); const packageJson = JSON.parse(fs.readFileSync('package.json')); delete packageJson.main; delete packageJson.module; fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2));"
 
-yarn run parcel build --no-cache --dist-dir=docs demo/index.html
+mkdir -p docs/demo
+yarn run parcel build --no-cache --dist-dir=docs/demo demo/index.html
+
+# Add .nojekyll for GitHub Pages
+touch docs/.nojekyll
 
 # Restore the original package.json
 echo "$packageJson" > package.json
